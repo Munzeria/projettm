ed5bb6124a12d1082e700bbb1da618645dfd29f9
Munzeria <quentincornez50@gmail.com>
2020-05-12 15:22:55 +0200

    smol scrin fix

--- ticket.js
+++ ticket.js
@@ -1,48 +1,67 @@
+// enfant, etudiant, adulte, senior
 var qte_ticket=[0,0,0,0];
+var prix_ticket=[0,0,0,0];
 
+//	Cree la table avec les differents prix
 function createTable(retour){
+	prix_ticket[0]=retour.tarifEnfant;
+	prix_ticket[1]=retour.tarifEtudiant;
+	prix_ticket[2]=retour.tarifAdulte;
+	prix_ticket[3]=retour.tarifSenior;
 	var table="";
 	//	Titres des colonnes
 	table+="<div class='table-responsive-sm' id='getTable'>"+
 		"<table class='table table-hover'>"+
 		"<thead><tr class='d-flex'>"+
-		"<th class='col-4' scope='col'>Type de ticket</th>"+
+		"<th class='col-3' scope='col'>Ticket</th>"+
 		"<th class='col-2' scope='col'>Prix</th>"+
-		"<th class='col-4' scope='col'>Quantité</th>"+
-		"<th class='col-2' scope='col'>Prix total</th>"+
+		"<th class='col-5' scope='col'>Quantité</th>"+
+		"<th class='col-2' scope='col'>Total</th>"+
 		"</tr></thead>";
 	//	ENFANT
 	table+="<tr class='d-flex'>"+
-		"<td class='col-4'>Enfant</td>"+
+		"<td class='col-3'>Enfant</td>"+
 		"<td class='col-2'>"+retour.tarifEnfant+"€</td>"+
-		"<td class='col-4'><input type='button' value='-' class='btn btn-sm' onclick='decrementer(0,qte_ticket,"+retour.tarifEnfant+")'/>    <output id=quantite_0>"+qte_ticket[0]+"</output><input type='button' value='+' class='btn btn-sm' onclick='incrementer(0,qte_ticket,"+retour.tarifEnfant+")'/></td>"+
-		"<td class='ligne col-2' id='total_0'>"+qte_ticket[0]*retour.tarifEnfant+"€</td>"+
+		"<td class='col-5'><input type='button' value='-' class='btn btn-sm' onclick='decrementer(0,qte_ticket,"+retour.tarifEnfant+")'/>	<output id=quantite_0>"+qte_ticket[0]+"</output>	<input type='button' value='+' class='btn btn-sm' onclick='incrementer(0,qte_ticket,"+retour.tarifEnfant+")'/></td>"+
+		"<td class='ligne col-2 text-right' id='total_0'>"+qte_ticket[0]*retour.tarifEnfant+"€</td>"+
 		"</tr>";
 	//	ETUDIANT
 	table+="<tr class='d-flex'>"+
-		"<td class='col-4'>Etudiant</td>"+
+		"<td class='col-3'>Etudiant</td>"+
 		"<td class='col-2'>"+retour.tarifEtudiant+"€</td>"+
-		"<td class='col-4'><input type='button' value='-' class='btn btn-sm' onclick='decrementer(1,qte_ticket,"+retour.tarifEtudiant+")'/>    <output id=quantite_1>"+qte_ticket[1]+"</output>    <input type='button' value='+' class='btn btn-sm' onclick='incrementer(1,qte_ticket,"+retour.tarifEtudiant+")'/></td>"+
-		"<td class='ligne col-2' id='total_1'>"+qte_ticket[1]*retour.tarifEtudiant+"€</td>"+
+		"<td class='col-5'><input type='button' value='-' class='btn btn-sm' onclick='decrementer(1,qte_ticket,"+retour.tarifEtudiant+")'/>    <output id=quantite_1>"+qte_ticket[1]+"</output>    <input type='button' value='+' class='btn btn-sm' onclick='incrementer(1,qte_ticket,"+retour.tarifEtudiant+")'/></td>"+
+		"<td class='ligne col-2 text-right' id='total_1'>"+qte_ticket[1]*retour.tarifEtudiant+"€</td>"+
 		"</tr>";
 	//	ADULTE
 	table+="<tr class='d-flex'>"+
-		"<td class='col-4'>Adulte</td>"+
+		"<td class='col-3'>Adulte</td>"+
 		"<td class='col-2'>"+retour.tarifAdulte+"€</td>"+
-		"<td class='col-4'><input type='button' value='-' class='btn btn-sm' onclick='decrementer(2,qte_ticket,"+retour.tarifAdulte+")'/>    <output id=quantite_2>"+qte_ticket[2]+"</output>    <input type='button' value='+' class='btn btn-sm' onclick='incrementer(2,qte_ticket,"+retour.tarifAdulte+")'/></td>"+
-		"<td class='ligne col-2' id='total_2'>"+qte_ticket[2]*retour.tarifAdulte+"€</td>"+
+		"<td class='col-5'><input type='button' value='-' class='btn btn-sm' onclick='decrementer(2,qte_ticket,"+retour.tarifAdulte+")'/>    <output id=quantite_2>"+qte_ticket[2]+"</output>    <input type='button' value='+' class='btn btn-sm' onclick='incrementer(2,qte_ticket,"+retour.tarifAdulte+")'/></td>"+
+		"<td class='ligne col-2 text-right' id='total_2'>"+qte_ticket[2]*retour.tarifAdulte+"€</td>"+
 		"</tr>";
 	//	SENIOR
 	table+="<tr class='d-flex'>"+
-		"<td class='col-4'>Senior</td>"+
+		"<td class='col-3'>Senior</td>"+
 		"<td class='col-2'>"+retour.tarifSenior+"€</td>"+
-		"<td class='col-4'><input type='button' value='-' class='btn btn-sm' onclick='decrementer(3,qte_ticket,"+retour.tarifSenior+")'/>    <output id=quantite_3>"+qte_ticket[3]+"</output>    <input type='button' value='+' class='btn btn-sm' onclick='incrementer(3,qte_ticket,"+retour.tarifSenior+")'/></td>"+
-		"<td class='ligne col-2' id='total_3'>"+qte_ticket[3]*retour.tarifSenior+"€</td>"+
+		"<td class='col-5'><input type='button' value='-' class='btn btn-sm' onclick='decrementer(3,qte_ticket,"+retour.tarifSenior+")'/>    <output id=quantite_3>"+qte_ticket[3]+"</output>    <input type='button' value='+' class='btn btn-sm' onclick='incrementer(3,qte_ticket,"+retour.tarifSenior+")'/></td>"+
+		"<td class='ligne col-2 text-right' id='total_3'>"+qte_ticket[3]*retour.tarifSenior+"€</td>"+
 		"</tr>";
+	//	Ligne total ?
+	table+="<tr class='d-flex'>"+
+		"<th class='col-4'>Total</th>"+
+		"<th class='col-1'/>"+
+		"<th class='col-4'/>"+
+		"<th class='col-3 text-right'><output id=total>"+totalTickets()+"</output>€</tr>"
 	table+="</table></div>";
 	return table;
 }
 
+//	Renvoie le total du ticket
+function totalTickets(){
+	return prix_ticket[0] * qte_ticket[0] + prix_ticket[1] * qte_ticket[1] + prix_ticket[2] * qte_ticket[2] + prix_ticket[3] * qte_ticket[3];
+}
+
+//	Decremente la valeur de ticket id
 function decrementer(id, table, prix){
 	if(table[id]<=0) return;
 	table[id]--;
@@ -57,6 +76,7 @@ function incrementer(id, table, prix){
 function actualiser(id, table, prix){
 	document.getElementById("quantite_"+id).innerHTML=table[id];
 	document.getElementById("total_"+id).innerHTML=table[id]*prix+"€";
+	document.getElementById("total").innerHTML=totalTickets();
 }
 
 function reset(){
